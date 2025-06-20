5d9ee3a5803d6d4c726155284aa05fff88a871e8
darksider225 <abhayrangari897@gmail.com>
2025-06-20 10:15:12 -0400

    update files

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -11,6 +11,24 @@ const globalConst = "I'm a global constant";
     const blockConst = "I'm a blocked-scoped const";
 }
 
+{
+    //Block scope reassign
+    var blockVar1 = "I'm a blocked scoped var";
+    blockVar1 = "blockVar1 Reassigned";
+    console.log(blockVar1);
+    let blockLet1 = "I'm a blocked-scoped let";
+    blockLet1 = "blockLet1 Reassigned";
+    console.log(blockLet1);
+    const blockConst1 = "I'm a blocked-scoped const";
+    blockConst1 = "blockConst1 Reassigned";
+    console.log(blockConst1);
+}
+
+console.log(blockVar1);
+console.log(blockLet1); 
+console.log(blockConst1);
+
+
 // Global Scope Console log
 console.log(globalVar);
 console.log(globalLet);
